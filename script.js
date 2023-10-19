c3c9dfd1f40066dd0730ba4eb19d787dc75f67ff
James Hunter <jamessahunter@gmail.com>
2023-10-19 13:09:00 -0700

    fill template literal

--- script.js
+++ script.js
@@ -30,7 +30,29 @@ const questions = [
 ]
 
 function generateHTML(response){
-    return `${response.name}`;
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
+    <section class="card">
+    <h1>${response.name}</h1>
+    <h2>${response.location}</h2>
+    <p>${response.bio}</p>
+    <h3>Contact</h3>
+    <a href="${response.LinkedIn}" target="_blank">LinkedIn</a></br>
+    <a href="${response.GitHub}" target="_blank">GitHub</a>
+    </section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+    </html>`;
 }
 
 function promptUser(data){
